1897f2c9a8b09fd59b201e94797ff400b4929041
SimasDei <simondeiman@gmail.com>
2019-04-07 08:28:48 +0300

    add Quote Components, new quote functionality, emit event from child component, then push quote to main app data array

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <app-new-quote @quoteAdded="newQuote"/>
+    <app-quote-grid :quotes="quotes"/>
+  </div>
 </template>
 
 <script>
-export default {};
+import appQuoteGrid from "./components/QuoteGrid.vue";
+import appNewQuote from "./components/NewQuote.vue";
+export default {
+  data: function() {
+    return {
+      maxQuotes: 10,
+      quotes: ["Smart fellow, he felt smart"]
+    };
+  },
+  components: {
+    appQuoteGrid,
+    appNewQuote
+  },
+  methods: {
+    newQuote(quote) {
+      this.quotes.push(quote);
+    }
+  }
+};
 </script>
 
 <style>
